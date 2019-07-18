3a0ad938f6b2fcfaf699ebe6ebf4ee991321630e
马思远 <39672285+fanhexiaoseng@users.noreply.github.com>
2019-07-18 10:11:07 +0800

    Create 分割列表.py

---
+++ 知识点/code/分割列表.py
@@ -0,0 +1,18 @@
+# _*_ coding : utf-8 _*_
+# 按逗号分隔列表
+
+a = [1, 2, 3, 4, 5, 6]
+# b = ",".join(str(n)for n in a)
+# print(b)
+print(str(i)for i in a)
+
+
+def a():
+    print("蓝蓝的天空！")
+    
+    
+if __name__ == '__main__':
+    a(）
+
+#<generator object <genexpr> at 0x0000000003953E08>
+蓝蓝的天空！    
