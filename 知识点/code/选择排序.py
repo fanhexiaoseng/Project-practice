069e12a6c2a4cb3d6b893348b526c556b5fa034f
马思远 <39672285+fanhexiaoseng@users.noreply.github.com>
2019-07-18 10:23:06 +0800

    Create 选择排序.py

---
+++ 知识点/code/选择排序.py
@@ -0,0 +1,24 @@
+# _*_ coding :utf-8 _*_
+
+
+A = [64, 25, 12, 22, 11]
+
+for i in range(len(A)):
+
+    min_idx = i
+    for j in range(i + 1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+print("排序后的数组：")
+for i in range(len(A)):
+    print("%d" % A[i])
+    
+#排序后的数组：
+11
+12
+22
+25
+64
