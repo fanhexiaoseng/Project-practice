ec743d9e663371ade3c348bcf9256f349e74a890
马思远 <39672285+fanhexiaoseng@users.noreply.github.com>
2019-07-19 15:28:36 +0800

    Create 类装饰器.py

---
+++ 知识点/code/类装饰器.py
@@ -0,0 +1,38 @@
+import time
+import functools
+
+
+class DelayFunc:
+    def __init__(self, duration, func):
+        self.duration = duration
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        print('Wait for {self.duration} seconds...')
+        time.sleep(self.duration)
+        return self.func(*args, **kwargs)
+
+    def eager_call(self, *args, **kwargs):
+        print('Call without delay')
+        return self.func(*args, **kwargs)
+
+
+def delay(duration):
+    """装饰器：推迟某个函数的执行。同时提供 .eager_call 方法立即执行
+    """
+
+    # 此处为了避免定义额外函数，直接使用 functools.partial 帮助构造
+
+    # DelayFunc 实例
+    return functools.partial(DelayFunc, duration)
+
+
+@delay(duration=2)
+def add(a, b):
+    return a + b
+    # 这次调用将会延迟 2 秒
+
+
+add(1, 2)
+# 这次调用将会立即执行
+add.eager_call(1, 2)
